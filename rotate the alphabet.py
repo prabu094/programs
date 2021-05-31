b7927a8210d455beb4fbd87e06c6c32f89ad8258
nithi308 <65847223+nithi308@users.noreply.github.com>
2021-05-31 07:45:26 +0530

    Create rotate the alphabet.py

---
+++ rotate the alphabet.py
@@ -0,0 +1,14 @@
+def rotate_chr(c):
+    rot_by = 5
+    c = c.lower()
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    # Keep punctuation and whitespace
+    if c not in alphabet:
+        return c
+    rotated_pos = ord(c) + rot_by
+    # If the rotation is inside the alphabet
+    if rotated_pos <= ord(alphabet[-1]):
+        return chr(rotated_pos)
+    # If the rotation goes beyond the alphabet
+    return chr(rotated_pos - len(alphabet))
+print(rotate_chr("a"))
