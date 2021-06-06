61603e456d5cf3dbbdcc49e35823d537c3c07631
Nithish kumar <nithishkumarravi308@gmail.com>
2021-06-06 16:47:17 +0530

    Create prime numbers in descending order.py

---
+++ prime numbers in descending order.py
@@ -0,0 +1,22 @@
+#Python code to print the nearest prime number of a number 
+import math 
+def isPrime(num): 
+    if(num==2 or num==3 or num==5 or num==7):	 
+        prime=True 
+    elif(num%2==0 or num%3==0 or num%5==0 or num%7==0): 
+    	prime=False	 
+    elif(math.ceil(math.sqrt(num))-math.floor(math.sqrt(num))==0): 
+        prime=False 
+    else:	 
+        prime=True 
+ 
+    return(prime) 
+#------------------------------------ 
+#pnear-first prime number below n 
+#snear-first prime number after n 
+n=int(input("Enter the number:")) 
+for i in range(n-1,1,-1): 
+    if(isPrime(i)): 
+        pnear=i
+        print(pnear)
+
