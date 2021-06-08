acb10ac73b9e4ef1734786afa6d8aee5a654da83
Nithish kumar <nithishkumarravi308@gmail.com>
2021-06-08 17:00:07 +0530

    Create Quick_sort.py

---
+++ Quick_sort.py
@@ -0,0 +1,10 @@
+def quicksort(x):
+      if len(x) < 2:
+          return x
+      else:
+          pivot = x[0]
+          less = [i for i in x[1:] if i <= pivot]
+          greater = [i for i in x[1:] if i > pivot]
+          return quicksort(less) + [pivot] + quicksort(greater)
+arr=[34,76,2,78,2,4,1,8,54,32]
+print(quicksort(arr))
