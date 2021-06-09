778482e51527ac0b8af792e69def0af535b45388
Nithish kumar <nithishkumarravi308@gmail.com>
2021-06-09 15:56:09 +0530

    Create Triangle_rangoli.py

---
+++ Triangle_rangoli.py
@@ -0,0 +1,8 @@
+def print_rangoli(size):
+    alp = 'abcdefghijklmnopqrstuvwxyz'
+    for i in range(size-1,-size,-1):
+        temp = '-'.join(alp[size-1:abs(i):-1]+alp[abs(i):size])
+        print(temp.center(4*size-3,'-'))
+if __name__ == '__main__':
+    n = int(raw_input())
+    print_rangoli(n)
