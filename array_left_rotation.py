29c6d1762e7b1b060270c4443e0c0b57699d80f4
Nithish kumar <nithishkumarravi308@gmail.com>
2021-06-13 08:49:58 +0530

    Create array_left_rotation.py

---
+++ array_left_rotation.py
@@ -0,0 +1,19 @@
+a = list(map(int,input("Enter Array Elements").split()))
+n = int(input("No. of rotation : "))
+for i in range(n):
+    first = a[0]
+    for j in range(len(a) - 1):
+        a[j] = a[j+1]
+    a[len(a) - 1] = first
+
+print(a)
+
+
+#To remove time complexity issues!
+
+
+def rotLeft(a, d):
+    # Write your code here
+    alist = list(a)
+    b = alist[d:]+alist[:d]
+    return b
