52f7f5ca732c94f0812b33d8b251514c19c8f645
Nithish kumar <nithishkumarravi308@gmail.com>
2021-06-14 13:41:15 +0530

    Create infix_to_prefix.py

---
+++ infix_to_prefix.py
@@ -0,0 +1,37 @@
+OPERATORS = ('+', '-', '*', '/', '(', ')', '^')  # set of operators
+
+PRIORITY = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}  # dictionary having priorities
+
+
+def infix_to_postfix(expression):
+    stack = []
+    output = ''
+    for ch in expression:
+        if ch not in OPERATORS:
+            output += ch
+        elif ch == '(':
+            stack.append('(')
+        elif ch == ')':
+            while stack and stack[-1] != '(':
+                output += stack.pop()
+            stack.pop()
+        else:
+            while stack and stack[-1] != '(' and PRIORITY[ch] <= PRIORITY[stack[-1]]:
+                output += stack.pop()
+            stack.append(ch)
+    while stack:
+        output += stack.pop()
+    return output
+
+
+def infix_to_prefix(expression):
+    s = expression[::-1]
+    s = s.replace('(', ')')
+    s = s.replace(')', '(')
+
+    return infix_to_postfix(s)[::-1]
+
+
+expression = input('Enter infix expression')
+print('infix expression: ', expression)
+print('prefix expression: ', infix_to_prefix(expression))
