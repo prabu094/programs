6a87e41a090e934815887a9d3be5e0ad9bd03c87
Nithish kumar <nithishkumarravi308@gmail.com>
2021-06-29 12:04:59 +0530

    Create median_of_array.py

---
+++ median_of_array.py
@@ -0,0 +1,9 @@
+import statistics
+
+lst = list(map(int, input().split()))
+
+print(statistics.median(lst))import statistics
+
+lst = list(map(int, input().split()))
+
+print(statistics.median(lst))
