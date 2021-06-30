9f7e831fe4dc2c2be474c1e11d3ff1af7c70028d
Nithish kumar <nithishkumarravi308@gmail.com>
2021-06-30 08:15:37 +0530

    Update last_modified_bowl.py

--- last_modified_bowl.py
+++ last_modified_bowl.py
@@ -5,6 +5,9 @@ num = int("".join(ll1)) + 1
 ll2 = list(i for i in str(num))
 
 for i in range(len(ll2)):
-    if ll1[i] != ll2[i]:
+    if len(ll1) != len(ll2):
+        print(0)
+        break
+    elif ll1[i] != ll2[i]:
         print(i + 1)
         break
